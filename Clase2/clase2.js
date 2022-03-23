490a65ba48c230e8a94794784abee673cd6d5464
Matias Guarna <matias.guarna@gmail.com>
2022-03-23 14:53:37 -0300

    1313

--- Clase2/clase2.js
+++ Clase2/clase2.js
@@ -39,6 +39,23 @@ function sumar(sum1, sum2) {
 let pruebaSuma = sumar(1, 2);
 imprimir(pruebaSuma);
 
+//Crear un multiplicador
+function crearMultiplicador(multip) {
+  return function (n) {
+    return multip * n;
+  };
+}
+
+//El parametro dentro de crearMultiplicador es el numero por el cual multiplica
+const doble = crearMultiplicador(2);
+//El parametro que se le pasa dentro es el numero a multiplicar
+imprimir(doble(3));
+const triple = crearMultiplicador(3);
+const cuadruple = crearMultiplicador(4);
+imprimir(doble(10));
+imprimir(triple(10));
+imprimir(cuadruple(10));
+
 //Ej de declarar funciones
 function mostrarPalabra(p) {
   console.log(p);
@@ -49,3 +66,13 @@ mostrarPalabra("palabrita");
 (function mostrarPalabra2(p) {
   console.log(p);
 })("chau");
+
+//Closure ejemplo
+function crearGritarNombre(nombre){
+  const signos = "!!!";
+  return function () {
+    console.log(`${nombre}${signos}`);
+  }
+}
+const gritarMatias = crearGritarNombre("matias");
+gritarMatias();
