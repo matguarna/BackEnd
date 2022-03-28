d88b08c09ce1cb4900673ccef971a78a7ca42a7c
Matias Guarna <matias.guarna@gmail.com>
2022-03-28 16:11:06 -0300

    3 3s

--- Clase3/clase3.js
+++ Clase3/clase3.js
@@ -15,4 +15,85 @@ function crearSaludar() {
 const saludito = crearSaludar();
 saludito("capo");
 
-friusao
+//Funcion de primer orden
+function mostrarMuchasVeces(cantVeces, funcionX) {
+  for (let i = 0; i < cantVeces; i++) {
+    funcionX(); //funcion que se ejecutará
+  }
+}
+
+function mostrarLaFecha() {
+  console.log(Date.now());
+}
+//mostrarLaFecha();
+mostrarMuchasVeces(5, mostrarLaFecha);
+
+//Funcion con argumentos/parametros
+function ejMuchasVeces(funcionXX, cantidadVeces, argumentos) {
+  for (let i = 0; i < cantidadVeces; i++) {
+    funcionXX(argumentos);
+  }
+}
+ejMuchasVeces(
+  function (n) {
+    console.log(n + 1);
+  },
+  3,
+  5
+);
+
+//Iterar objetos
+function ejParaCadaArg(fn, vals) {
+  for (const val of vals) {
+    fn(val);
+  }
+}
+ejParaCadaArg(console.log, [1, 2, 3, 4, 5]);
+
+for (let i = 0; i < 3; i++) {
+  console.log(i);
+}
+
+//FOR OF
+const col = [1, 2, 3, 4, 5];
+for (const num of col) {
+  console.log(num + 1);
+}
+
+//FOR IN
+const col2 = {
+  [0]: "a",
+  [1]: "b",
+  [2]: "c",
+};
+
+const diccionario = { nombre: "matias", apellido: "guarna", rol: "alumno" };
+for (const cosa in diccionario) {
+  console.log(cosa); //Imprime nombre, apellido y rol (las claves)
+}
+
+console.log(Object.keys(diccionario));
+console.log(Object.values(diccionario));
+console.log(Object.entries(diccionario));
+console.table(Object.entries(diccionario));
+
+for (const valor of Object.values(diccionario)) {
+  console.log(valor);
+}
+
+//Ej declaracion de un array
+const nums = [1, 2, 3];
+nums[20]; //Indica que nums tiene 20 posiciones
+nums[6] = "HOLA"; //Le da el valor HOLA en la posicion 6
+console.log(nums);
+console.log(nums.length); //Imprime 7
+
+//Desestructuracion de un array
+const letras = ["a", "b", "c"];
+const [primeraLetra, segundaLetra, terceraLetra, cuartaLetra] = letras;
+
+console.log(primeraLetra);
+console.log(segundaLetra);
+console.log(terceraLetra);
+console.log(cuartaLetra); //Indefinido porque no existe
+console.log(letras[0]);
