9de543d3b916389ebd4e40688aaeed91244a7118
Matias Guarna <matias.guarna@gmail.com>
2022-03-29 20:13:00 -0300

    promises

--- Clase4/src/archivos.js
+++ Clase4/src/archivos.js
@@ -30,39 +30,60 @@ const fs = require("fs");
 // });
 
 //Misma operacion de arriba pero mas corta
-try {
-  fs.writeFileSync("prueba.txt", "probando...".repeat(100));
-  console.log("terminé de escribir");
-  const result = fs.readFileSync("prueba.txt", "utf-8");
-  console.log("terminé de leer");
-  console.log(result);
-} catch (err) {
-  console.log(err);
-} finally {
-  console.log("terminé finally"); //Se ejecuta siempre, independientemente de si se realizó la tarea o entró en el catch
-}
+
+// try {
+//   fs.writeFileSync("prueba.txt", "probando...".repeat(100));
+//   console.log("terminé de escribir");
+//   const result = fs.readFileSync("prueba.txt", "utf-8");
+//   console.log("terminé de leer");
+//   console.log(result);
+// } catch (err) {
+//   console.log(err);
+// } finally {
+//   console.log("terminé finally"); //Se ejecuta siempre, independientemente de si se realizó la tarea o entró en el catch
+// }
 
 console.log("terminé ultimo");
 
 //PROMESAS. devuelven un resultado a futuro
-const promesa = fs.writeFile("prueba.txt", "probando...".repeat(100));
-promesa
-  .then(() => {
-    const promesa2 = fs.readFile("prueba.txt", "utf-8");
-    return promesa2;
-  })
-  .then(() => {
-    console.log("terminé promesa");
-  });
+// const promesa = fs.writeFile("prueba.txt", "probando...".repeat(100));
+// promesa
+//   .then(() => {
+//     const promesa2 = fs.readFile("prueba.txt", "utf-8");
+//     return promesa2;
+//   })
+//   .then(() => {
+//     console.log("terminé promesa");
+//   });
 
 //Otro ej
-fs.writeFile("prueba.txt", "probando...".repeat(100))
-  .then(() => {
-    return fs.readFile("prueba.txt", "utf-8");
-  })
-  .then(() => {
-    console.log("terminé ej 2");
-  })
-  .catch((err) => {
+// fs.promises
+//   .writeFile("prueba.txt", "probando...".repeat(10))
+//   .then(() => {
+//     return fs.promises.readFile("prueba.txt", "utf-8");
+//   })
+//   .then((datos) => {
+//     console.log(datos);
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   })
+//   .finally(() => {
+//     console.log("Terminé finally");
+//   });
+
+//AWAIT hace que la promesa espere a que se resuelva antes de seguir con la proxima linea de codigo.
+async function trabajarConArchivos() {
+  try {
+    await fs.promises.writeFile("prueba.txt", "probando...".repeat(10));
+    const datos = await fs.promises.readFile("prueba.txt", "utf-8");
+    console.log(datos);
+  } catch (err) {
     console.log(err);
-  });
+  } finally {
+    console.log("Terminé finally async");
+  }
+}
+trabajarConArchivos();
+
+console.log(10 + 10);
