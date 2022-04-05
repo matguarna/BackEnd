350413cea8526f096ef6187cbf21de7e449a8dc9
Matias Guarna <matias.guarna@gmail.com>
2022-04-05 17:34:34 -0300

    6

--- Clase5/main.js
+++ Clase5/main.js
@@ -1,1 +1,8 @@
+//Desestructuracion de la libreria
+const { color, log } = require("console-log-colors");
+const { red, green, cyan } = color;
+
 console.log("HOLAAAAAAA");
+console.log(red("color.red('text')"));
+console.log(cyan("CYANNNNNNNNNNNNNNN"));
+console.log(green("vergreeeen"));
