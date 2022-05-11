8cf550ea8256f3c78e94ff2e694d305a4e0b8a0d
Matias Guarna <matias.guarna@gmail.com>
2022-05-11 17:50:02 -0300

    13

--- Clase13/babel.js
+++ Clase13/babel.js
@@ -1,8 +1,6 @@
-{Babel
-Es un transpilador que permite transformar nuestro codigo JS de ultima generacion a JS que cualquier navegador o version de NodeJs entienda.
-Funciona mediante plugins, con el siguiente plugin podemos decirle que transforme las arrow function de ECMAScript2015 a funciones normales:
-```
-babel-plugin-transform-es2015-arrow-functions
-```
-Desde la web: https://babeljs.io/ podemos copiar el código a traducir y devuelve la versión traducida.
-}
+//Ordenar un array
+const array = [1, 5, 4, 8, "a", "%"];
+console.log(array);
+
+const array2 = array.sort();
+console.log(array2);
